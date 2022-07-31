7b68f954f2bc0e877ac3540e8c2a41d4024cdb6b
Mary Stankevich <marrycitty@mail.ru>
2022-07-31 17:30:32 +0200

    Update CodeRefactor.java

--- src/main/java/hw_16/task_5/CodeRefactor.java
+++ src/main/java/hw_16/task_5/CodeRefactor.java
@@ -1,5 +1,6 @@
 package hw_16.task_5;
 
+//+
 public class CodeRefactor {
     public static void main(String[] args) {
         Website website = new Website("http://www.onliner.by");
@@ -17,7 +18,7 @@ class Website {
     private WebsiteType type;
 
     public Website(String url) {
-
+//лишняя строка
         this(url, WebsiteType.NEWS);
     }
 
